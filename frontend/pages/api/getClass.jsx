b689f538f7d8ede3bd9dc7c7b09b1c3f9c900394
Samyok Nepal <sam@yok.dev>
2022-11-04 10:22:09 -0500

    ⭐️ add class api endpoint

--- frontend/pages/api/getClass.jsx
+++ frontend/pages/api/getClass.jsx
@@ -9,30 +9,38 @@ const dbPath = path.resolve(process.cwd(), "../ProcessedData.db");
 const db = new Database(dbPath, OPEN_READONLY);
 
 export default function handler(req, res) {
-  db.all(
-    "SELECT * FROM classdistribution WHERE REPLACE(class_name, ' ', '') = REPLACE($class_name, ' ' , '')",
-    { $class_name: req.query.class_name },
-    (err, rows) => {
-      if (err) {
-        res.status(500).json({
-          success: false,
-          message: "Database error, please try again later.",
-          error: err.message,
-        });
-        return;
-      }
+  if (!req.query.class_name) {
+    res.status(400).json({ error: "Missing class_name in query string" });
+    return;
+  }
 
-      if (rows.length) {
-        res.status(200).json({
-          success: true,
-          data: rows[0],
-        });
-      } else {
-        res.status(404).json({
-          success: false,
-          message: "Class Not Found",
-        });
-      }
+  const sql =
+    "SELECT * FROM classdistribution WHERE REPLACE(class_name, ' ', '') = REPLACE($class_name, ' ' , '')";
+
+  const params = {
+    $class_name: req.query.class_name,
+  };
+
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(500).json({
+        success: false,
+        message: "Database error, please try again later.",
+        error: err.message,
+      });
+      return;
     }
-  );
+
+    if (rows.length) {
+      res.status(200).json({
+        success: true,
+        data: rows[0],
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        message: "Class Not Found",
+      });
+    }
+  });
 }
