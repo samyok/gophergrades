8c6db69201826850c880bc463137355a92a20e70
Kanishk-K <kanishkkacholia@gmail.com>
2022-11-05 15:33:44 -0500

    Applied Professor Traceback function to Cleaning tool.

--- data-app/clean_csv.py
+++ data-app/clean_csv.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import json
+import numpy as np
 import requests
 import re
 
@@ -7,29 +7,28 @@ RUNS = 0
 
 
 def fetch_unknown_prof(x):
-    global RUNS
-    if RUNS < 20:
-        dept = x["SUBJECT"].iloc[0]
-        catalog_nbr = x["CATALOG_NBR"].iloc[0]
-        term = str(x["TERM"].iloc[0])
-        section = x["CLASS_SECTION"].iloc[0]
-        level=catalog_nbr[0]
-        professor="null"
+    dept = x["SUBJECT"].iloc[0]
+    catalog_nbr = x["CATALOG_NBR"].iloc[0]
+    term = str(x["TERM"].iloc[0])
+    section = x["CLASS_SECTION"].iloc[0]
+    level=catalog_nbr[0]
+    professor="Unknown Professor"
 
-        with requests.get("http://classinfo.umn.edu/?subject="+dept+"&term="+term+"&level="+level+"&json=1") as url:
-            print("url: http://classinfo.umn.edu/?subject="+dept+"&term="+term+"&level="+level+"&json=1")
-            try:
-                data=url.json()
-                for key in data:
-                    if re.search((term+"-"+dept+"-"+catalog_nbr),key)!=None:
-                        classComp=data[key]["Class Component"]
-                        if classComp=="Lecture" or classComp=="LEC": #Are there any other ways they specifiy lectures?
-                            professor=re.findall("\\t(.*)",data[key]["Instructor Data"])[0]
-            except ValueError:
-                print("Json malformed, icky!")
+    with requests.get("http://classinfo.umn.edu/?subject="+dept+"&term="+term+"&level="+level+"&json=1") as url:
+        print("url: http://classinfo.umn.edu/?subject="+dept+"&term="+term+"&level="+level+"&json=1")
+        try:
+            data=url.json()
+            for key in data:
+                if re.search((term+"-"+dept+"-"+catalog_nbr),key)!=None:
+                    classComp=data[key]["Class Component"]
+                    if classComp=="Lecture" or classComp=="LEC": #Are there any other ways they specifiy lectures?
+                        professor=re.findall("\\t(.*)",data[key]["Instructor Data"])[0]
+        except ValueError:
+            print("Json malformed, icky!")
 
-        print(f"{dept} {catalog_nbr} section {section} taught on term {term} which is a level {catalog_nbr[0]} class and was taught by {professor}.")
-        RUNS += 1
+    print(f"{dept} {catalog_nbr} section {section} taught on term {term} which is a level {catalog_nbr[0]} class and was taught by {professor}.")
+    x["HR_NAME"] = professor
+    return x
 
 df = pd.read_csv("raw_data.csv",dtype={6:str,14:str})
 # Unneeded Data
@@ -47,6 +46,6 @@ df = df.astype({"TERM":int})
 # Write class name as the proper full name that students are accustomed to.
 df["FULL_NAME"] = df["SUBJECT"] + ' ' + df["CATALOG_NBR"]
 # Replace unknown professor values with either a correct name or "Unknown Professor"
-df[df["HR_NAME"].isnull()].groupby(["TERM","FULL_NAME","CLASS_SECTION"]).apply(fetch_unknown_prof)
-df["HR_NAME"] = df["HR_NAME"].fillna("Unknown Professor")
+df.groupby(["TERM","FULL_NAME","CLASS_SECTION"]).apply(lambda x: x if x["HR_NAME"].iloc[0] == np.nan else fetch_unknown_prof(x))
+print(df[df["HR_NAME"].isnull()])
 df.to_csv("cleaned_data.csv",index=False)
