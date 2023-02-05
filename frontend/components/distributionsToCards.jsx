4899651f16f3868fb3c85b5b42545613e728c765
Samyok Nepal <sam@yok.dev>
2023-02-05 12:58:52 -0600

    🥹 sexy toggle

--- frontend/components/distributionsToCards.jsx
+++ frontend/components/distributionsToCards.jsx
@@ -1,5 +1,16 @@
-import { Badge, HStack, Stat, Tag, Text, VStack } from "@chakra-ui/react";
+import {
+  Badge,
+  Box,
+  Collapse,
+  HStack,
+  IconButton,
+  Tag,
+  Text,
+  VStack,
+  useDisclosure,
+} from "@chakra-ui/react";
 import React from "react";
+import { ChevronDownIcon, ChevronRightIcon } from "@chakra-ui/icons";
 import Stats from "./Stats";
 import Card from "./Card";
 import { letterToColor, termToName } from "../lib/letterTo";
@@ -13,90 +24,113 @@ const sortingFunctions = {
   RECENCY: (array) => array.sort((a, b) => (a.term < b.term ? 1 : -1)),
 };
 
-const renderSingleDistribution = (dist, isMobile, isStatic) => {
+const SingleDistribution = ({ dist, isMobile, isStatic }) => {
+  const { isOpen, onToggle } = useDisclosure();
   const title = dist.title ?? "Unknown";
   return (
-    <Card
-      key={dist.distribution_id}
-      isSummary={dist.isSummary}
-      href={isStatic ? "#" : dist.href}
-      isStatic={isStatic}
-    >
-      <HStack
-        justify={"center"}
-        align={"center"}
-        width={"100%"}
-        flexWrap={"wrap"}
+    <Box pos={"relative"} width={"full"}>
+      <Card
+        key={dist.distribution_id}
+        isSummary={dist.isSummary}
+        href={isStatic ? "#" : dist.href}
+        isStatic={isStatic}
       >
-        <VStack align={"start"} flexGrow={1} pb={4} width={"50%"}>
-          {!dist.hideTitle && (
-            <Text fontSize={dist.isSummary ? "3xl" : "lg"} fontWeight={"bold"}>
-              {(!isStatic || !dist.isSummary) && title}
-            </Text>
-          )}
-          {dist.subtitle && (
-            <Text fontSize={"md"} color={"gray.600"} fontWeight={"500"}>
-              {dist.subtitle}
-            </Text>
-          )}
-          <HStack>
-            {dist.averageGPA > 0 && (
+        <HStack
+          justify={"center"}
+          align={"center"}
+          width={"100%"}
+          flexWrap={"wrap"}
+        >
+          <VStack align={"start"} flexGrow={1} pb={4} width={"50%"}>
+            {!dist.hideTitle && (
+              <Text
+                fontSize={dist.isSummary ? "3xl" : "lg"}
+                fontWeight={"bold"}
+              >
+                {(!isStatic || !dist.isSummary) && title}
+              </Text>
+            )}
+            {dist.subtitle && (
+              <Text fontSize={"md"} color={"gray.600"} fontWeight={"500"}>
+                {dist.subtitle}
+              </Text>
+            )}
+            <HStack>
+              {dist.averageGPA > 0 && (
+                <Tag
+                  textAlign={"center"}
+                  colorScheme={letterToColor(dist.averageGradeLetter)}
+                  py={1}
+                >
+                  {dist.averageGradeLetter} Average ({dist.averageGPA})
+                </Tag>
+              )}
               <Tag
                 textAlign={"center"}
-                colorScheme={letterToColor(dist.averageGradeLetter)}
+                colorScheme={letterToColor(dist.mostStudents)}
                 py={1}
               >
-                {dist.averageGradeLetter} Average ({dist.averageGPA})
+                Most Common: {dist.mostStudents} ({dist.mostStudentsPercent}
+                %)
               </Tag>
+            </HStack>
+            {dist.info && (
+              <Text fontSize={"sm"} color={"gray.600"} pt={2}>
+                {dist.info}
+              </Text>
             )}
-            <Tag
-              textAlign={"center"}
-              colorScheme={letterToColor(dist.mostStudents)}
-              py={1}
-            >
-              Most Common: {dist.mostStudents} ({dist.mostStudentsPercent}
-              %)
-            </Tag>
-          </HStack>
-          {dist.info && (
-            <Text fontSize={"sm"} color={"gray.600"} pt={2}>
-              {dist.info}
-            </Text>
-          )}
-        </VStack>
+          </VStack>
 
-        <VStack>
-          <HStack>
-            <Badge>{dist.students} students</Badge>
-            {dist.terms && (
-              <Badge>
-                {dist.terms.length} term{dist.terms.length > 1 && "s"}
-              </Badge>
-            )}
-          </HStack>
-          <HStack>
-            {dist.BarChart}
-            {dist.averageGPA > 0 && dist.Component}
-          </HStack>
-        </VStack>
-      </HStack>
-      {dist.terms && dist.terms.length > 1 && (
-        <VStack pt={3} spacing={3}>
-          {dist.terms.map((term) =>
-            renderSingleDistribution(
-              {
-                ...term,
-                ...Stats({ distribution: term, isMobile }),
-                subtitle: termToName(term.term),
-                hideTitle: true,
-              },
-              isMobile,
-              isStatic
-            )
-          )}
-        </VStack>
+          <VStack>
+            <HStack>
+              <Badge>{dist.students} students</Badge>
+              {dist.terms && (
+                <Badge>
+                  {dist.terms.length} term{dist.terms.length > 1 && "s"}
+                </Badge>
+              )}
+            </HStack>
+            <HStack>
+              {dist.BarChart}
+              {dist.averageGPA > 0 && dist.Component}
+            </HStack>
+          </VStack>
+        </HStack>
+        {dist.terms && dist.terms.length > 1 && (
+          <Collapse in={isOpen} animateOpacity>
+            <VStack spacing={3} p={2} pt={3}>
+              {dist.terms.map((term) => (
+                <SingleDistribution
+                  dist={{
+                    ...term,
+                    ...Stats({ distribution: term, isMobile }),
+                    subtitle: termToName(term.term),
+                    hideTitle: true,
+                  }}
+                  isMobile={isMobile}
+                  isStatic={isStatic}
+                />
+              ))}
+            </VStack>
+          </Collapse>
+        )}
+      </Card>
+      {!isStatic && dist.terms && dist.terms.length > 1 && (
+        <IconButton
+          pos={"absolute"}
+          size={"xs"}
+          top={3.5}
+          left={0}
+          aria-label={"toggle dropdown"}
+          variant={"ghost"}
+          colorScheme={"blackAlpha"}
+          rounded={"full"}
+          onClick={onToggle}
+        >
+          {isOpen ? <ChevronDownIcon /> : <ChevronRightIcon />}
+        </IconButton>
       )}
-    </Card>
+    </Box>
   );
 };
 
@@ -114,4 +148,11 @@ export const distributionsToCards = (
         ...distribution,
         ...Stats({ distribution, isMobile }),
       }))
-  ).map((dist) => renderSingleDistribution(dist, isMobile, isStatic));
+  ).map((dist) => (
+    <SingleDistribution
+      key={dist.id ?? dist.title}
+      dist={dist}
+      isMobile={isMobile}
+      isStatic={isStatic}
+    />
+  ));
