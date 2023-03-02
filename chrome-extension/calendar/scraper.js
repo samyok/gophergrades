13ab6b6d1747d4ce398d92e946ac3e3e1bde8ba2
William Walker <walk0950@umn.edu>
2023-03-02 13:28:53 -0600

    💏 migrate lots of "date" `string`s to actual `Date`s

--- chrome-extension/calendar/scraper.js
+++ chrome-extension/calendar/scraper.js
@@ -11,23 +11,23 @@
  * @returns {string} // yyyy-mm-dd
  *  */ 
 const formatDateForURL = (date) => {
-  let year = date.getFullYear()
-  let month = date.getMonth() + 1 // why is january 0????
-  let day = date.getDate()
+  let year = date.getUTCFullYear() // utc to remove annoying timezone shift
+  let month = date.getUTCMonth() + 1 // why is january 0????
+  let day = date.getUTCDate()
   return [year, ('0' + month).slice(-2), ('0' + day).slice(-2)].join("-")
 }
 
 /**
  * Returns a list of json objects containing all the meetings in a given week
  * Json includes course name, number, date (a single date!), time, room, etc. 
- * @param {string} [date="unset"] // a day during the week in question (Let's say the Sunday.), in format "yyyy-mm-dd", WITH DASHES. "unset" gives the current week
+ * @param {string} [dateString="unset"] // a day during the week in question (Let's say the Sunday.), in format "yyyy-mm-dd", WITH DASHES. "unset" gives the current week
  * @returns {Array} meetingObjectArray
  */
-const weekToJson = async (date="unset") => {
+const weekToJson = async (dateString="unset") => {
   // appends the date info to our base url
   let url = "https://www.myu.umn.edu/psp/psprd/EMPLOYEE/CAMP/s/WEBLIB_IS_DS.ISCRIPT1.FieldFormula.IScript_DrawSection?group=UM_SSS&section=UM_SSS_ACAD_SCHEDULE&pslnk=1&cmd=smartnav" // the base url
-  if (date != "unset") {
-    url = url.concat("&effdt=", date)
+  if (dateString != "unset") {
+    url = url.concat("&effdt=", dateString)
   }
   // create a (queryable!) DOM element from the url
   let HTMLText;
@@ -48,12 +48,20 @@ const weekToJson = async (date="unset") => {
       continue
     }
 
+    // utility func to parse yyyymmdd into Date object
+    let parseDate = (dateString) => {
+      year = dateString.slice(0, 4)
+      month = dateString.slice(4, 6)
+      day = dateString.slice(6, 8)
+      return new Date(Date.UTC(year, month-1, day)) // month-1 ew
+    }
+
     let classDetails = meetingEl.querySelector(".myu_calendar-class-details").innerHTML
                         .replace(/\n/g,"").split("<br>") // regex is to get rid of random newlines that are in there for some reason
     meetingObjects.push({
       "term"        : meetingEl.getAttribute("data-strm"), // in format `xyyx', where `yy` is the year and `xx` = `13` is spring, `19` is fall
       "courseNum"   : meetingEl.getAttribute("data-class-nbr"),
-      "date"        : meetingEl.getAttribute("data-fulldate"),
+      "date"        : parseDate(meetingEl.getAttribute("data-fulldate")),
       "meetingType" : classDetails[0],
       "timeRange"   : classDetails[1],
       "room"        : classDetails[2].replace(/^ | $/g,""), // strip off the leading and trailing space with regex
@@ -78,15 +86,28 @@ const generalClassInfo = async (term, courseNum, institution="UMNTC") => {
   const baseURL = "https://www.myu.umn.edu/psp/psprd/EMPLOYEE/CAMP/s/WEBLIB_IS_DS.ISCRIPT1.FieldFormula.IScript_DrawSection?group=UM_SSS&section=UM_SSS_CLASS_DETAIL&cmd=smartnav"
   let url = baseURL.concat("&STRM=", term, "&CLASS_NBR=", courseNum, "&INSTITUTION=", institution)
 
+  // set up element to parse
   var el = document.createElement('html')
   await fetch(url).then(r => r.text()).then(r => el.innerHTML = r)
+
+  // dateRange parsing
+  let dateRangeString = el.querySelector('[data-th="Meeting Dates"]').innerText
+  let dateRange = dateRangeString.split(" - ").map(dateString => {
+    let splitDate = dateString.split("/")
+    let year = splitDate[2]
+    let month = splitDate[0]
+    let day = splitDate[1]
+    return new Date(Date.UTC(year, month-1, day))
+  }
+  )
 
   return ({
     "term"        : term,
     "courseNum"   : courseNum,
     "institution" : institution,
-    "dateRange"   : el.querySelector('[data-th="Meeting Dates"]').innerText,
-    "daysOfWeek"  : el.querySelector('[data-th="Days and Times"]').innerText.slice(0,7), // everything before character 8
+    "dateRange"   : dateRange,
+    // "dateRangeString" : dateRangeString, for debugging
+    "daysOfWeek"  : el.querySelector('[data-th="Days and Times"]').innerText.slice(0,7), // everything before character 8 // TODO: put this in better format? esp bc Thursday ("Th") messes stuff up I expect
     "timeRange"   : el.querySelector('[data-th="Days and Times"]').innerText.slice(8), // everything after character 8 (inclusive)
     // "meetingType" : el.querySelector('[data-th="Meeting Dates"]').innerText, // uh not doing this rn
     "room"        : el.querySelector('[data-th="Room"]').innerText,
@@ -123,20 +144,20 @@ for (let i = 0; i < courseNums.length; i++) {
   coursesInfo.push(await generalClassInfo(term, courseNums[i], institution))
 }
 
-// 2.5: parse the dateRange into nice dates that we can loop through
-dates = coursesInfo[0].dateRange.split('-')
-startDate = new Date(dates[0])
-endDate = new Date(dates[1]) // is it actually this simple?
+// 3: loop through week by week and do ...stuff
+startDate = endDate = coursesInfo[0].dateRange[0]
+endDate = coursesInfo[0].dateRange[1]
+endDate.setDate(endDate.getDate() + 7) // pad an extra week onto endDate so we can be sure we got everything
 
 // for loop from startDate to endDate. step size is one week
 // `date` is a date lying in the week of interest
 // this is so cursed
 let weeks = []
-for (let date = startDate; date <= endDate; date.setDate(date.getDate() + 7)) { // maybe we want to go a little farther than endDate to be safe?
-  let currentWeek = await weekToJson(formatDateForURL(date))
+for (let date = startDate; date <= endDate; date.setDate(date.getDate() + 7)) { // the reason we need to pad endDate
+  let currentWeekData = await weekToJson(formatDateForURL(date))
   // now um somehow check for when a class should be happening, but isn't (e.g. break/holiday)
   // ??? think about this later
-  weeks.push(currentWeek)
+  weeks.push(currentWeekData)
 }
 
 // 3. 
