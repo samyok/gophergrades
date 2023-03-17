4f4e54ad01fe151f007712b3433dc1ae1c877f87
sammoerbitz <moerb001@umn.edu>
2023-03-16 20:09:24 -0500

    Button now applies when Academics page is loaded

--- chrome-extension/calendar/calendar.js
+++ chrome-extension/calendar/calendar.js
@@ -613,28 +613,31 @@ const htmlToElement = (html) => {
     return template.content.firstChild;
 };
 
+//True if the myu button is already added
+let ButtonIsAdded = false;
+
 /**
  * Appends a new button on the MyU academics tab. 
  */
 const appendButton = () => {
-    const newDiv = htmlToElement(buttonTemplate); //This should be a new element node?
-    
-    //This is the div that contains buttons "View Calendar" "List View" and "Textbooks (UMTC)"
-    const calendarDiv = document.getElementsByClassName("myu_btn-group col-lg-12")[0];
-    
-    //A div that holds calendarDiv inside of it
-    const parentDiv = document.getElementsByClassName("row")[4];
-    
-    if(calendarDiv != null) {
-        
-        parentDiv.insertBefore(newDiv, calendarDiv.nextSibling);
+  const newDiv = htmlToElement(buttonTemplate); //This is a new element node
+      
+  //A div that holds calendarDiv inside of it
+  const parentDiv = document.getElementsByClassName("row")[4];
 
-        //Apply following 
-        newDiv.querySelector("button").addEventListener("click", buttonBody)
-        
-    }else{
-        console.log("Button not working");
-    }
+  //This is the div that contains buttons "View Calendar" "List View" and "Textbooks (UMTC)"
+  const calendarDiv = document.getElementsByClassName("myu_btn-group col-lg-12")[0];
+  
+  if(calendarDiv != null) {
+      
+      parentDiv.insertBefore(newDiv, calendarDiv.nextSibling);
+
+      //Apply following 
+      newDiv.querySelector("button").addEventListener("click", () => {
+
+      });
+      ButtonIsAdded= true; 
+  }
 }
 
 /**
@@ -655,4 +658,17 @@ const buttonBody = async () => {
   // console.log(createRecurringVEVENT(c, []))
 }
 
-setTimeout(appendButton, 5000); //Wait 5 seconds after load before applying button. There has to be a better way 
+const onLoad = () => {
+  const app = document.querySelector("body");
+
+  const appObserver = new MutationObserver((mutations) => {
+    if(!ButtonIsAdded) appendButton();
+    if(document.querySelector("#homepageTabList li.active").innerText?.trim() != 'ACADEMICS') ButtonIsAdded = false;
+
+  });
+
+  appObserver.observe(app, {childList: true, subtree: true});
+};
+
+
+window.addEventListener("load", onLoad);
