7b9a3cf861c8a9e9d35d4b708495858144c3500d
Samyok Nepal <sam@yok.dev>
2023-04-13 18:18:26 -0500

    🍽️ chrome extension add to build script

--- bin/build-extensions.js
+++ bin/build-extensions.js
@@ -1,11 +1,27 @@
-const exec = require('child_process').exec;
-const colors = require('./lib/colors');
+const exec = require("child_process").exec;
+const colors = require("./lib/colors");
 
-exec('web-ext build -s firefox-extension/ --overwrite-dest', (err, stdout, stderr) => {
-  if (err) {
-    console.log(colors.FgRed, 'Failed to build firefox-extension');
-    return;
+exec(
+  "web-ext build -s firefox-extension/ --overwrite-dest",
+  (err, stdout, stderr) => {
+    if (err) {
+      console.log(colors.FgRed, "Failed to build firefox-extension");
+      return;
+    }
+    console.log(stdout);
+    console.log(colors.FgGreen, "Successfully built firefox-extension\n");
   }
-  console.log(stdout)
-  console.log(colors.FgGreen, 'Successfully built firefox-extension\n');
-});
+);
+
+// zip the chrome extension folder for distribution
+
+exec(
+  "zip -r web-ext-artifacts/chrome-extension.zip chrome-extension",
+  (err, stdout, stderr) => {
+    if (err) {
+      console.log(colors.FgRed, "Failed to zip chrome-extension");
+      return;
+    }
+    console.log(colors.FgGreen, "Successfully zipped chrome-extension\n");
+  }
+);
