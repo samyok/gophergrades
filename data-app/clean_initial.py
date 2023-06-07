c97971bfa7aa8c93fb12ff21cddb161f4ee81d88
Kanishk-K <kanishkkacholia@gmail.com>
2023-06-07 21:13:34 +0530

    ⚕ Combined datasets, generate distributions.

--- data-app/clean_initial.py
+++ data-app/clean_initial.py
@@ -139,7 +139,7 @@ THIS WILL LIKELY NOT STAY CONSISTENT.
 """
 
 
-df = pd.read_csv("CLASS_DATA/FALL2022_raw_data.csv",dtype={"CLASS_SECTION":str,"Unnamed: 14":str})
+df = pd.read_csv("CLASS_DATA/SPR2023_raw_data.csv",dtype={"CLASS_SECTION":str,"Unnamed: 14":str})
 # Unneeded Data
 del df["INSTITUTION"]
 # del df["CAMPUS"]
@@ -159,4 +159,4 @@ df["CLASS_SECTION"] = df["CLASS_SECTION"].apply(lambda x: x.zfill(3))
 df = df.groupby(["TERM","FULL_NAME","CLASS_SECTION"],group_keys=False).apply(fetch_unknown_prof)
 df["HR_NAME"] = df["HR_NAME"].apply(format_name)
 # print(df)
-df.to_csv("CLASS_DATA/FALL2022_cleaned_data.csv",index=False)
+df.to_csv("CLASS_DATA/SPR2023_cleaned_data.csv",index=False)
