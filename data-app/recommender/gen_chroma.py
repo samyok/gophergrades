7dc70044341324003a44e89f5ac655b978c8282e
William Walker <24390822+superstealthysheep@users.noreply.github.com>
2023-09-25 02:48:09 -0500

    📚 add chromadb generation and query scripts

--- data-app/recommender/gen_chroma.py
+++ data-app/recommender/gen_chroma.py
@@ -7,9 +7,11 @@ from chromadb.utils import embedding_functions
 import openai
 from dotenv import load_dotenv
 import os
+import sqlite3
 
 CHROMA_COLLECTION_NAME = "fall23"
 OPENAI_MODEL_NAME = "text-embedding-ada-002"
+DB_PATH = "../../ProcessedData.db"
 
 load_dotenv(".env") # figure out best place to put this
 # OPENAI_KEY = os.getenv("OPENAI_KEY")
@@ -21,10 +23,40 @@ load_dotenv(".env") # figure out best place to put this
 # will eventually be properly hosted. for now, localhost
 chroma_client = chromadb.HttpClient(host="localhost", port=8090)
 
-collection = chroma_client.create_collection(
+# could use chroma_client.upsert() method if that's of any utility?
+chroma_client.delete_collection(name=CHROMA_COLLECTION_NAME) # TODO: catch error if collection doesn't exist?
+collection = chroma_client.get_or_create_collection(
     name=CHROMA_COLLECTION_NAME,
     # for now, just use default embedding func. 
     # embedding_function=openai_ef 
 )
+
+sql_connection = sqlite3.connect(DB_PATH)
+cursor = sql_connection.cursor()
+cursor.execute("SELECT id, class_name, class_desc, onestop_desc FROM classdistribution")
+
+desc = cursor.description
+column_names = [col[0] for col in desc]
+
+# create a nice dict keyed by column names
+rows = [dict(zip(column_names, row)) for row in cursor.fetchall()]
+
+sql_connection.close()
+
+metadatas = []
+documents = []
+ids = []
+
+for row in rows:
+    if row["onestop_desc"] == None:
+        print(f"found empty onestop description for {row['class_name']} (id: {row['id']}). Skipping embedding...")
+        continue
+
+    metadatas.append({"class_name":row["class_name"], "class_desc":row["class_desc"]})
+    #   metadatas.append({"subject":subject,"level":catalog_nbr[0],"instructor":x500,"title":title})
+    documents.append(row["onestop_desc"])
+    ids.append(str(row["id"])) # should we use class_name here instead?
+
+collection.add(metadatas=metadatas, documents=documents, ids=ids)
 
 # @doggu pls merge in sql reading thing?
