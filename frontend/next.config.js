0b622b3ad5409ee63e06e122c68b8166dbbbe5f5
Kanishk-K <kanishkkacholia@gmail.com>
2025-12-06 10:56:38 -0600

    Upgrade next to v15

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -15,7 +15,6 @@ const withMDX = require("@next/mdx")({
 const nextConfig = {
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
   reactStrictMode: true,
-  swcMinify: true,
   rewrites: async () => {
     return [
       {
